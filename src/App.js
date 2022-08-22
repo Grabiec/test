56129e0dffa7a6127ca25a8a643e450ca3383ef7
Grabiec <pawel.grabiec.contact@gmail.com>
2022-08-22 09:57:46 +0200

    zad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
 	return (
 		<div>
 			<h1>Hello world </h1>
 			<button>hej</button>
-			<p>co tam</p>
+			<p>co tm</p>
+			<Button variant="contained">Hello World</Button>;
 		</div>
 	)
 }
