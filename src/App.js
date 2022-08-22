8ec4299e0110f893fffe5f6db67fe7d9d0f5e28a
Grabiec <111655127+Grabiec@users.noreply.github.com>
2022-08-22 10:13:49 +0200

    Revert "zad"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css'
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-
 
 function App() {
 	return (
 		<div>
 			<h1>Hello world </h1>
 			<button>hej</button>
-			<p>co tm</p>
-			<Button variant="contained">Hello World</Button>;
-			<svg data-testid="DeleteIcon"></svg>
+			<p>co tam</p>
 		</div>
 	)
 }
