78c1bea2cd5e0a422909f4f4ca24308385447b65
Grabiec <pawel.grabiec.contact@gmail.com>
2022-08-26 11:38:04 +0200

    Add post post-listv2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import List from './import.js/list'
 import NewAdd from './send/newadd'
-import ButtonAppBar from './import.js/appBar'
 function App() {
 	return (
 		<div>
 			<List />
-			<ButtonAppBar />
 			<NewAdd />
 		</div>
 	)
